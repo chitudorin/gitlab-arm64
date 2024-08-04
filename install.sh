7d5b7fa1950b4d066db1596593e84e18be0b8700
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2024-08-04 23:55:30 +0300

    Add arm64 platform flag

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ TARGETARCH=arm64
 DOWNLOAD_URL_arm64=https://packages.gitlab.com/gitlab/gitlab-ce/packages/ubuntu/jammy/gitlab-ce_$1-ce.0_arm64.deb/download.deb
 EOF
 
-docker build -t gitlab-arm:$1 .
+docker build --platform linux/arm64 -t gitlab-arm:$1 .
 
 cd ../..
 rm -rf omnibus-gitlab
